f299bea14a3747aa67ddba29c9d3a8bf68278867
Mahfuza Humayra Mohona <mhmohona@gmail.com>
2023-12-08 18:15:49 +0600

    update setup file

--- 100-level-AsyncAPI-Intro/setup_node.sh
+++ 100-level-AsyncAPI-Intro/setup_node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install necessary prerequisites
-echo "We need to install Nodejs and NPM first. Stay strong!"
+echo "We are going to install Nodejs and NPM first."
 curl --silent -sL https://deb.nodesource.com/setup_current.x -o /tmp/nodesource_setup.sh
 bash /tmp/nodesource_setup.sh
 apt-get install -y -q nodejs
@@ -9,4 +9,4 @@ apt-get install -y -q nodejs
 # Install AsyncAPI CLI
 echo "Installing AsyncAPI CLI..."
 npm install -g @asyncapi/cli
-echo "All set! Go ahead!"
+echo "All set!!"
