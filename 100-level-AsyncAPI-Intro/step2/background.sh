28a9be0f15d13a159b32ddbb770f29178559174d
Mahfuza Humayra Mohona <mhmohona@gmail.com>
2023-12-11 11:58:36 +0600

    update path

--- 100-level-AsyncAPI-Intro/step2/background.sh
+++ 100-level-AsyncAPI-Intro/step2/background.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Clone the specified repository
-git clone https://github.com/mhmohona/interactive-learning-path
+git clone https://github.com/asyncapi/learning-paths
 
 # Change directory into the cloned repository
-cd interactive-learning-path/asyncapi/step1/assets
+cd learning-path/100-level-AsyncAPI-Intro/step1/assets
 
 # Any additional background setup tasks can be added here
