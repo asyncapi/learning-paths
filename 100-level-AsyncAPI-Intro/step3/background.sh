5d3793ec41c04d32993e9cb2c5f1aa8eeda6bd5b
Mahfuza Humayra Mohona <mhmohona@gmail.com>
2023-12-11 12:04:51 +0600

    added background file

---
+++ 100-level-AsyncAPI-Intro/step3/background.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Clone the specified repository
+git clone https://github.com/asyncapi/learning-paths
+
+# Change directory into the cloned repository
+cd learning-path/100-level-AsyncAPI-Intro/step3/assets
+
+# Any additional background setup tasks can be added here
